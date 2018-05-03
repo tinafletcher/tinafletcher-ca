1d6be3b6c40487dbdcf3c3710d0201ad113dd438
Tina Fletcher <tina.fletcher@d2l.com>
2018-05-03 10:54:30 -0400

    Reorganization of assets into sub-folders

--- writingFunctions.js
+++ writingFunctions.js
@@ -1,11 +1,11 @@
 /* not used anymore */
-function toggleVisibility(id){
+/*function toggleVisibility(id){
   var article = document.getElementById(id);
   if(article.style.display == 'block')
     article.style.display='none';
   else
     article.style.display='block';
-}
+}*/
 
 function updateBlogwindow(id, location){
   var selectedArticle = document.getElementById(id).innerHTML;
